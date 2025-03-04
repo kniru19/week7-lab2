0e6a949c63db36138b60e912c020362ee3b6d8dc
kniru19 <kniru19@gmail.com>
2025-03-04 18:21:28 +0200

    first commit

---
+++ docs/sidebars.js
@@ -0,0 +1,20 @@
+---
+sidebar_label: 'Hi!'
+sidebar_position: 3
+---
+
+# Hello
+
+This is my **first Docusaurus document**!
+
+export default {
+    tutorialSidebar: [
+      'intro',
+      'hello',
+      {
+        type: 'category',
+        label: 'Tutorial',
+        items: ['tutorial-basics/create-a-document'],
+      },
+    ],
+  };
